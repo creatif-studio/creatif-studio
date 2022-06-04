417155f39d294457ddc94e5a124f81ab5fae9bd7
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2022-06-04 13:46:55 +0700

    styling: basic certified UI

--- landing/components/organisms/Landing/Certified.jsx
+++ landing/components/organisms/Landing/Certified.jsx
@@ -1,7 +1,30 @@
+import { Button, Container } from "components/atoms";
+import Link from "next/link";
 import React from "react";
 
 const Certified = () => {
-  return <div>Certified</div>;
+  return (
+    <div className="mt-10 py-20 bg-gradient-to-r from-[#2C2D2F] to-[#606062]">
+      <Container className="mx-auto grid grid-cols-3 gap-8">
+        <h2 className="col-span-1 text-4xl font-semibold font-display text-white leading-normal">
+          Get HashiCorp Certified
+        </h2>
+        <div className="col-span-2">
+          <p className="text-neutral-200 text-lg mb-10 leading-relaxed">
+            Cloud engineers can use the Terraform Associate exam from HashiCorp
+            to verify their basic infrastructure automation skills.
+          </p>
+          <Link href="/">
+            <a>
+              <Button className="mt-5" mode="primary" buttonSize="md">
+                Prepare & get certified
+              </Button>
+            </a>
+          </Link>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Certified;
