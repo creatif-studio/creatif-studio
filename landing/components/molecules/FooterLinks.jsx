4fca41f5dad0b4ed9e13e676a13dfdaecbcbefd4
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2022-06-04 19:48:26 +0700

    styling: basic landing footer tab UI

--- landing/components/molecules/FooterLinks.jsx
+++ landing/components/molecules/FooterLinks.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const FooterLinks = () => {
   return (
     <div className="py-3 mb-1 md:mb-0">
-      <Container className="mx-auto flex flex-col md:flex-row items-center justify-start">
+      <Container className="mx-auto flex flex-col md:flex-row items-center justify-start md:px-10 xl:px-0">
         <FooterLink>Overview</FooterLink>
         <FooterLink>Docs</FooterLink>
         <FooterLink>Extend</FooterLink>
