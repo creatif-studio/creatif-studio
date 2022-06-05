59194ea0cb61d27dc38b94fda0723958e669a43b
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2022-06-05 10:42:51 +0700

    chore: basic use case hero UI

--- landing/components/organisms/UseCase/Hero.jsx
+++ landing/components/organisms/UseCase/Hero.jsx
@@ -1,7 +1,31 @@
+import { Container } from "components/atoms";
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="max-w-practice mx-auto bg-usecase-hero-gradient py-32 relative z-0 rounded-b-md">
+      <img
+        src="/images/usecase-hero-pattern.svg"
+        alt="usecase hero pattern"
+        className="w-2/6 scale-95 absolute -bottom-10 left-0"
+      />
+      <Container className="mx-auto">
+        <div className="mx-auto max-w-fit">
+          <p className="text-sm uppercase font-display mb-7 font-medium">
+            Use Case
+          </p>
+          <h1 className="font-display text-4xl lg:text-5xl lg:leading-tight font-semibold mb-6 max-w-sm">
+            Multi Cloud Deployment
+          </h1>
+          <p className="text-neutral-600 text-lg max-w-md leading-8">
+            Deploy serverless functions with AWS Lambda, manage Microsoft Azure
+            Active Directory resources, provision a load balancer in Google
+            Cloud, and more.
+          </p>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Hero;
