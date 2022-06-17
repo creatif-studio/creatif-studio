4c58c540ff8c32a6b94f111114412cd5dd3edd66
Ilham Adiputra <ilhamadiputra4@gmail.com>
2022-06-17 18:56:39 +0700

    styling: fix landing hero not aligned correctly in 1366

--- landing/components/organisms/Landing/Hero.jsx
+++ landing/components/organisms/Landing/Hero.jsx
@@ -6,7 +6,7 @@ import { BsArrowRight } from "react-icons/bs";
 
 const Hero = () => {
   return (
-    <div className="pt-20 px-5 md:px-10 xl:px-0 md:pt-32 pb-16 lg:pb-28 lg:mx-40 md:bg-hero-pattern bg-no-repeat bg-right-top cursor-default">
+    <div className="pt-20 px-5 md:px-10 xl:px-0 md:pt-32 pb-16 lg:pb-28 2xl:mx-40 md:bg-hero-pattern bg-no-repeat bg-right-top cursor-default">
       <Container className="mx-auto flex flex-col md:px-0 md:flex-row gap-4 md:gap-20 xl:gap-8">
         <div className="max-w-lg">
           <h1 className="font-display text-4xl lg:text-5xl font-semibold leading-tight mb-2">
