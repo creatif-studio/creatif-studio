4ecb7319b25e4cefe9eff08f87eecc4758c0dd52
sgnd <sugandaletters@gmail.com>
2023-08-30 15:45:27 +0700

    finish hero

--- components/molecules/Landing/CreatifCloudCard.jsx
+++ components/molecules/Landing/CreatifCloudCard.jsx
@@ -5,17 +5,17 @@ const CreatifCloudCard = () => {
   return (
     <Card className="col-span-1 shadow bg-white border-neutral-200 h-full py-8 px-8 md:max-w-sm cursor-default opacity-0 animate-[move-to-top_0.5s_forwards_0.5s]">
       <h3 className="font-semibold font-display text-2xl">
-        Creatif Studio Pro
+        Join Our Growing Team
       </h3>
       <h4 className="text-sm font-semibold font-display my-3">
-        Managed Creatif Studio
+        Unlock Exciting Prospects
       </h4>
       <Button className="mt-5" mode="primary" buttonSize="md">
-        Try Creatif Studio Pro
+        Start Your Journey
       </Button>
       <p className="text-sm text-neutral-500 mt-8 leading-relaxed">
-        Creatif Studio Pro enables infrastructure automation for provisioning,
-        compliance, and management of any cloud, datacenter, and service.
+        We offers an opportunity to contribute in designing products, software
+        development, manual/automation testing, cloud infrastructure, and more.
       </p>
     </Card>
   );
