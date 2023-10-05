db366297a418d53d71206f9af549fcb474ae4600
sgnd <sugandaletters@gmail.com>
2023-10-05 15:05:12 +0700

    update portfolios

--- components/pages/landing-page/work.tsx
+++ components/pages/landing-page/work.tsx
@@ -2,15 +2,15 @@ import { BaseButton } from "@/components/atoms";
 import Image from "next/image";
 import React from "react";
 import activitylist from "@/assets/images/portfolios/activitylist.png";
-// import appseed from "@/assets/images/portfolios/appseed.png";
-// import bitzaro from "@/assets/images/portfolios/bitzario.png";
+import appseed from "@/assets/images/portfolios/appseed.png";
+import bitzaro from "@/assets/images/portfolios/bitzario.png";
 import clevis from "@/assets/images/portfolios/celvis.png";
-// import cmsmanajer from "@/assets/images/portfolios/cmsmanajer.png";
-// import cquant from "@/assets/images/portfolios/cquant.png";
+import cmsmanajer from "@/assets/images/portfolios/cmsmanajer.png";
+import cquant from "@/assets/images/portfolios/cquant.png";
 import deploypro from "@/assets/images/portfolios/deploypro.png";
-// import idrive from "@/assets/images/portfolios/idrive.png";
-// import kelsiem from "@/assets/images/portfolios/kelsiem.png";
-// import muunship from "@/assets/images/portfolios/muunship.png";
+import idrive from "@/assets/images/portfolios/idrive.png";
+import kelsiem from "@/assets/images/portfolios/kelsiem.png";
+import muunship from "@/assets/images/portfolios/muunship.png";
 import practiz from "@/assets/images/portfolios/practiz.png";
 // import bookedmd from "@/assets/images/portfolios/bookedmd.png";
 import Link from "next/link";
@@ -97,6 +97,111 @@ const WorkSection = () => {
             <p>Event Sharing Platform</p>
           </div>
         </div>
+
+        <div className="relative pr-5 carousel-item card-image lg:pr-0">
+          <Image
+            width={274}
+            height={389}
+            src={appseed}
+            className="rounded-box"
+            alt="image"
+          />
+          <div className="p-3 text-center overlay lg:p-0">
+            <p className="text-3xl font-semibold">Appseed Docs</p>
+            <p>Technical Documentation</p>
+          </div>
+        </div>
+        <div className="relative pr-5 carousel-item card-image lg:pr-0">
+          <Image
+            width={274}
+            height={389}
+            src={bitzaro}
+            className="rounded-box"
+            alt="image"
+          />
+          <div className="p-3 text-center overlay lg:p-0">
+            <p className="text-3xl font-semibold">Bitzaro</p>
+            <p>Payment Gateway for Web3</p>
+          </div>
+        </div>
+        <div className="relative pr-5 carousel-item card-image lg:pr-0">
+          <Image
+            width={274}
+            height={389}
+            src={cmsmanajer}
+            className="rounded-box"
+            alt="image"
+          />
+          <div className="p-3 text-center overlay lg:p-0">
+            <p className="text-3xl font-semibold">CMS Manajer</p>
+            <p>Heroku Replacement</p>
+          </div>
+        </div>
+        <div className="relative pr-5 carousel-item card-image lg:pr-0">
+          <Image
+            width={274}
+            height={389}
+            src={cquant}
+            className="rounded-box"
+            alt="image"
+          />
+          <div className="p-3 text-center overlay lg:p-0">
+            <p className="text-3xl font-semibold">cQuant</p>
+            <p>Ansible Script for Deployment</p>
+          </div>
+        </div>
+        <div className="relative pr-5 carousel-item card-image lg:pr-0">
+          <Image
+            width={274}
+            height={389}
+            src={idrive}
+            className="rounded-box"
+            alt="image"
+          />
+          <div className="p-3 text-center overlay lg:p-0">
+            <p className="text-3xl font-semibold">IDrive E2</p>
+            <p>S3 Compatible Storage</p>
+          </div>
+        </div>
+        <div className="relative pr-5 carousel-item card-image lg:pr-0">
+          <Image
+            width={274}
+            height={389}
+            src={kelsiem}
+            className="rounded-box"
+            alt="image"
+          />
+          <div className="p-3 text-center overlay lg:p-0">
+            <p className="text-3xl font-semibold">Kelsiem</p>
+            <p>Terraform for AWS Management</p>
+          </div>
+        </div>
+        <div className="relative pr-5 carousel-item card-image lg:pr-0">
+          <Image
+            width={274}
+            height={389}
+            src={muunship}
+            className="rounded-box"
+            alt="image"
+          />
+          <div className="p-3 text-center overlay lg:p-0">
+            <p className="text-3xl font-semibold">Muunship</p>
+            <p>Trading Platform for Crypto</p>
+          </div>
+        </div>
+        {/* <div className="relative pr-5 carousel-item card-image lg:pr-0">
+          <Image
+            width={274}
+            height={389}
+            src={bookedmd}
+            className="rounded-box"
+            alt="image"
+          />
+          <div className="p-3 text-center overlay lg:p-0">
+            <p className="text-3xl font-semibold">BookedMD</p>
+            <p>Doctor Finders</p>
+          </div>
+        </div> */}
       </div>
     </section>
   );
