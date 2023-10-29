5074037fbf25c2f0de3ae898c302948205e5ed67
rianllauo <rianllauo@gmail.com>
2023-10-29 21:47:35 +0700

    Add a new services in landing page

--- components/pages/landing-page/service.tsx
+++ components/pages/landing-page/service.tsx
@@ -79,7 +79,6 @@ const ServiceSection = () => {
   return (
     <div id="service">
       <section className="flex flex-col items-center w-full px-6 lg:px-24 pt-20 pb-2 ">
-        {/* md:mb-13 */}
         <div className="space-y-3 text-center mt-10">
           <h1 className="text-4xl font-semibold md:text-[42px]">
             What we can do for you
