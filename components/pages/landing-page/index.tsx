60bc7ccf2cc8e1cc79c72d8ab362b1baf75198ff
rianllauo <rianllauo@gmail.com>
2023-10-29 19:29:54 +0700

    Provide the title of the section and the distance from the navbar

--- components/pages/landing-page/index.tsx
+++ components/pages/landing-page/index.tsx
@@ -1,24 +1,13 @@
-import {
-  Creatif,
-  CreatifDark,
-  CreatifWhite,
-  HumburgerMenu,
-} from "@/assets/icons";
-import { BaseButton } from "@/components/atoms";
 import React from "react";
 import HeroSection from "./hero";
 import ServiceSection from "./service";
 import WorkSection from "./work";
 import TestimoniSection from "./testimoni";
-import FooterSection from "./footer";
-import { Brand } from "@/components/atoms/brand";
 import OurTeamSection from "./ourTeam";
-import Navbar from "./navbar";
 
 const LandingPage = () => {
   return (
     <div>
-      {/* <Navbar /> */}
       <HeroSection
         page="home"
         title="Creatif Studio"
@@ -32,7 +21,6 @@ const LandingPage = () => {
         <TestimoniSection />
         <OurTeamSection />
       </div>
-      {/* <FooterSection /> */}
     </div>
   );
 };
--- components/pages/landing-page/work.tsx
+++ components/pages/landing-page/work.tsx
@@ -13,36 +13,22 @@ import kelsiem from "@/assets/images/portfolios/kelsiem.png";
 import muunship from "@/assets/images/portfolios/muunship.png";
 import practiz from "@/assets/images/portfolios/practiz.png";
 import bookedmd from "@/assets/images/portfolios/booked.png";
-import Link from "next/link";
 
 const WorkSection = () => {
   return (
     <section
       id="portfolio"
-      className="flex flex-col items-center w-full lg:px-24"
+      className="flex flex-col items-center w-full lg:px-24 pt-30"
     >
       <div className="space-y-3 text-center">
         <h1 className="text-4xl font-semibold md:text-[42px]">
           Our amazing projects
         </h1>
         <p>
-          Our portfolio reflects years of hard work, dedication, and successful collaborations.
+          Our portfolio reflects years of hard work, dedication, and successful
+          collaborations.
         </p>
       </div>
-
-      {/* <div className="flex flex-col w-full gap-4 px-6 lg:px-0 py-13 md:flex-row md:justify-between">
-        <h1 className="text-4xl font-semibold capitalize md:text-[42px]">
-          {"Our Amazing Project"}
-        </h1>
-        <div className="space-y-6">
-          <p>
-            Our portfolio reflects years of hard work, dedication, and successful collaborations.
-          </p>
-          <Link href={"/portofolio"}>
-            <BaseButton variant="primary" label="See All Project" />
-          </Link>
-        </div>
-      </div> */}
 
       <div className="flex justify-between w-full gap-4 carousel carousel-center my-12">
         <div className="relative ml-6 carousel-item card-image lg:ml-0">
